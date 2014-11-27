53dacdde8c1b1de4dcf1e5be058f651d0ce1fa1c
Daniel McDavid <mcdaviddj@gmail.com>
2014-11-27 12:46:16 -0500

    seperated CSS

--- index.php
+++ index.php
@@ -1,121 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-body {
-    background-color: #990012;
-}
-#container
-{
-	margin: 0 auto;
-	width: 1000px;
-	background: #990012;
-}
-
-#header
-{
-	background: #990012;
-	padding: 20px;
-}
-
-#header h1 { margin: 0; }
-
-#navigation
-{
-	float: left;
-	width: 1000px;
-	background: #000000;
-}
-
-#navigation ul
-{
-	margin: 0;
-	padding: 0;
-background: #990012;
-}
-
-#navigation ul li
-{
-	list-style-type: none;
-	display: inline;
-background: #990012;
-}
-
-#navigation li a
-{
-	display: block;
-	float: left;
-	padding: 5px 10px;
-	color: #fff;
-	text-decoration: none;
-	border-right: 1px solid #990012;;
-}
-
-#navigation li a:hover { background: #990012; }
-
-#content-container
-{
-	float: left;
-	width: 1000px;
-	background: #EDB321 url(/wp-content/uploads/layout-three-fixed-background.gif) repeat-y 100% 0;
-}
-
-#section-navigation
-{
-	float: left;
-	width: 160px;
-	padding: 20px 0;
-	margin: 0 20px;
-	display: inline;
-}
-
-#section-navigation ul
-{
-	margin: 0;
-	padding: 0;
-}
-
-#section-navigation ul li
-{
-	margin: 0 0 1em;
-	padding: 0;
-	list-style-type: none;
-}
-
-#content
-{
-	float: left;
-	width: 500px;
-	padding: 20px 0;
-	margin: 0 0 0 30px;
-background: #EDB321;
-}
-
-#content h2 { margin: 0; }
-
-#aside
-{
-	float: right;
-	width: 200px;
-	padding: 20px 0;
-	margin: 0 20px 0 0;
-	display: inline;
-
-}
-
-#aside h3 { margin: 0; }
-
-#footer
-{
-	clear: left;
-	background: #000000;
-	text-align: right;
-	padding: 20px;
-	height: 1%;
-
-}
-}
-</style>
 </head>
 <body>
 
@@ -123,20 +8,14 @@ background: #EDB321;
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-<title>www.rotw.de.ki | Template</title>
+<title>Average Joe's Fitness Studio</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="style.css" type="text/css" />
-<!--[if IE ]><style type="text/css">
-.title {
-padding-top:112px;
-}
-</style>
-<![endif]-->
 </head>
 <body>
 <div id="all">
 <div id="header">
-<h1><center><img src="http://ep.yimg.com/ay/yhst-39482811324711/average-joes-31.gif"style="width:400px;height:200px"> </h1>
+<h1><center><img src="images/average-joes-31.gif"style="width:400px;height:200px"> </h1>
 
 
 </div>
@@ -166,7 +45,7 @@ padding-top:112px;
 		</div>
 		<div id="content">
 			<h2>
-				<center><img src="http://photos1.blogger.com/blogger/8144/1382/1600/Average-Joe4.gif "style="width:200px;height:200px">
+				<center><img src="images/Average-Joe4.gif "style="width:200px;height:200px">
 			</h2>
 			<p>
 				I'm Peter LaFleur, owner and operator of Average Joe's gym and I'm here to tell you, you're perfect the way you are. But if you feel like losing a few pounds, eating healthier and making a few friends in the process, then Joe's is the place for you. Don't forget, youth dodge ball classes are forming right now. So learn a great game like it's supposed to be played. 
@@ -182,7 +61,7 @@ padding-top:112px;
 			<h3>
 
 
-				<img src="http://farmstrong.ca/wp-content/uploads/2012/04/CnJ.png "style="width:200px;height:250px" 
+				<img src="http://farmstrong.ca/wp-content/uploads/2012/04/CnJ.png"style="width:200px;height:250px" 
 			</h3>
 			<p>
 				
