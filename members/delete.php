9ee9a46c8d350e033ed4c754de772fc3feb24b90
Daniel McDavid <mcdaviddj@gmail.com>
2014-12-01 08:12:47 -0500

    added delete member

---
+++ members/delete.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+if($_SESSION["loggedIn"] != true) {
+        echo("Access denied!");
+        exit();
+    }
+
+$host="localhost"; // Host name
+$username="apache"; // Mysql username
+$password="info630!"; // Mysql password
+$db_name="info630"; // Database name
+$tbl_name="Members"; // Table name
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect");
+mysql_select_db("$db_name")or die("cannot select DB");
+
+$myuserID=$_POST['deleteUser'];
+
+$sql="DELETE FROM $tbl_name WHERE memberNumber = '$myuserID'";
+
+$result = mysql_query($sql);
+
+if($result==1){
+	header("location:view.php");
+}
+
+else {
+	echo "An error occured";
+	}
+?>
--- members/view.php
+++ members/view.php
@@ -12,52 +12,57 @@ if($_SESSION["loggedIn"] != true) {
 <body>
 <h1>View Members</h1>
 <?php
-$host="localhost"; // Host name
-$username="apache"; // Mysql username
-$password="info630!"; // Mysql password
-$db_name="info630"; // Database name
-$tbl_name="Members"; // Table name
+    $host="localhost"; // Host name
+    $username="apache"; // Mysql username
+    $password="info630!"; // Mysql password
+    $db_name="info630"; // Database name
+    $tbl_name="Members"; // Table name
 
-if (!mysql_connect($host, $username, $password))
-    die("Can't connect to database");
+    if (!mysql_connect($host, $username, $password))
+        die("Can't connect to database");
 
-if (!mysql_select_db($db_name))
-    die("Can't select database");
+    if (!mysql_select_db($db_name))
+        die("Can't select database");
 
-// sending query
-$result = mysql_query("SELECT memberNumber AS 'Member ID',memberName AS 'Name',role AS Role,memberPhone AS 'Phone Number' FROM {$tbl_name}");
-if (!$result) {
-    die("Query to show fields from table failed");
-}
+    // sending query
+    $result = mysql_query("SELECT memberNumber AS 'Member ID',memberName AS 'Name',role AS Role,memberPhone AS 'Phone Number' FROM {$tbl_name}");
+    if (!$result) {
+        die("Query to show fields from table failed");
+    }
 
-$fields_num = mysql_num_fields($result);
-echo 'Return to <a href="../menu/main.php">Main Menu</a><br>';
-echo '<a href="add.php">Add</a> new member<br>';
-echo "Current Members {$table}";
-echo "<table border='1'><tr>";
-// printing table headers
-for($i=0; $i<$fields_num; $i++)
-{
-    $field = mysql_fetch_field($result);
-    echo "<td>{$field->name}</td>";
-}
-echo "</tr>\n";
-// printing table rows
-while($row = mysql_fetch_row($result))
-{
-    echo "<tr>";
-
-    // $row is array... foreach( .. ) puts every element
-    // of $row to $cell variable
-    foreach($row as $cell)
-        echo "<td>$cell</td>";
+    $fields_num = mysql_num_fields($result);
+    echo 'Return to <a href="../menu/main.php">Main Menu</a><br>';
+    echo '<a href="add.php">Add</a> new member<br>';
+    echo "Current Members {$table}";
+    echo "<table border='1'><tr>";
+    
+    // printing table headers
+    for($i=0; $i<$fields_num; $i++){
+        $field = mysql_fetch_field($result);
+        echo "<td>{$field->name}</td>";
+        }
 
     echo "</tr>\n";
-}
 
-mysql_free_result($result);
+    // printing table rows
+    while($row = mysql_fetch_row($result)){
+        echo "<tr>";
 
+        // $row is array... foreach( .. ) puts every element
+        // of $row to $cell variable
+        foreach($row as $cell)
+            echo "<td>$cell</td>";
+        echo "</tr>\n";
+        }
+
+    echo "</table>";
+    mysql_free_result($result);
 ?>
-
+<br>
+Delete User ID:
+<form name="delete" method="post" action="delete.php">
+<input name="deleteUser" type="text" id="deleteUser">
+<input type="submit" name="Submit" value="Submit">
+</form>
 </body>
 </html>
