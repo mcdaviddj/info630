55b1f7a2b108a0595fa6b4762c50f3939e210010
Daniel McDavid <mcdaviddj@gmail.com>
2014-12-01 20:27:39 -0500

    fixed menu

--- menu/main.php
+++ menu/main.php
@@ -12,8 +12,8 @@
 </head>
 <body>
 <h1>Main Menu</h1>
-<h2>Welcome, <?=$_SESSION["login_name"]?>!</h2>
-<h3>Your current role is: <?=$_SESSION["login_role"]?></h3>
+<h2>Welcome, <?=$_SESSION["login_name"];?>!</h2>
+<h3>Your current role is: <?=$_SESSION["login_role"];?></h3>
 
 <!-- Shows various menu options based on session variable 'login_role' -->
 <?php 
