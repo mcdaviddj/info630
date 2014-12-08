41676ce1cd1ed7333f09b3261a52067e02cfcaef
Daniel McDavid <mcdaviddj@gmail.com>
2014-12-07 20:47:44 -0500

    Added CSS to Payment Screen

--- payments.php
+++ payments.php
@@ -8,8 +8,35 @@ if($_SESSION["loggedIn"] != true) {
 <html>
 <head>
 <title>Average Joe's | Process Payment</title>
+    <link rel="stylesheet" href="/style.css" type="text/css" />
+
 </head>
+
 <body>
+    <div id="all">
+<div id="header">
+<h1><center><img src="/images/average-joes-31.gif"style="width:400px;height:200px"> </h1>
+
+
+</div>
+</div>
+<div id="container">
+    <div id="header">
+    
+    </div>
+    <div id="navigation">
+        <ul>
+            <li><a href="/index.php">Home</a></li>
+            <li><a href="../logout.php">Logout</a></li>
+        </ul>
+    </div>
+    <div id="content-container">
+        <div id="section-navigation">
+            <ul>
+                
+            </ul>
+        </div>
+        <div id="content">
 <table width="300" border="0" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
     <tr>
         <form name="form1" method="post" action="checkpayment.php">
@@ -40,6 +67,6 @@ if($_SESSION["loggedIn"] != true) {
         </form>
     </tr>
 </table>
-
+</div>
 </body>
 </html>
